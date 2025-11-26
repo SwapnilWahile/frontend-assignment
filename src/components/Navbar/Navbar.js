4438572f3f31beec9f31922901dee91b943c4998
SwapnilWahile <swapnilwahile24@gmail.com>
2025-11-26 15:40:29 +0530

    nav update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,19 +2,41 @@ import React from "react";
 import "./Navbar.scss";
 import logo from "../../assets/images/hero/logo.png";
 
-
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo"><img src={logo} alt="app-logo" height={49} width={125}></img></div>
+      <div className="logo">
+        <img src={logo} alt="app-logo" height={49} width={125}></img>
+      </div>
 
-      <ul className="nav-links">
+      {/* <ul className="nav-links">
         <li>Home</li>
         <li>Client</li>
         <li>Services</li>
         <li>Project</li>
         <li>About</li>
         <li>Contact</li>
+      </ul> */}
+
+      <ul className="nav-links">
+        <li>
+          Home <span className="chevron">›</span>
+        </li>
+        <li>
+          Client <span className="chevron">›</span>
+        </li>
+        <li>
+          Services <span className="chevron">›</span>
+        </li>
+        <li>
+          Project <span className="chevron">›</span>
+        </li>
+        <li>
+          About <span className="chevron">›</span>
+        </li>
+        <li>
+          Contact <span className="chevron">›</span>
+        </li>
       </ul>
 
       <button className="nav-btn hamburger">
